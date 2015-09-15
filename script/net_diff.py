58c3bc3e2aaa831d8d612a0ddea1c28a9569b807
parastuffs <parastuffs@gmail.com>
2015-09-15 14:43:21 +0200

    Doc

--- script/net_diff.py
+++ script/net_diff.py
@@ -1,32 +1,44 @@
+""" net_diff.py
+2015-09-15
+
+Compares the network list in Nets.out and WLnets.out.
+Lists all the nets that are in Nets.out but not in WLnets.out.
+"""
+
 INPUT_FILE_DIR = "../input_files/"
-forbiddenLines = ["-", "#", "NAME", "NET", "Steiner"]
+FORBIDDEN_LINES = ["-", "#", "NAME", "NET", "Steiner"] # Strings beginning unwanted lines
 
 
 nets = list()
 WLnets = list()
 missingNets = list()
 
+# Get the nets
 with open(INPUT_FILE_DIR+'Nets.out', 'r') as netsInput:
 	nets = netsInput.read().splitlines()
 
+# Get the WLnets...
 with open(INPUT_FILE_DIR+'WLnets.out', 'r') as WLnetsInput:
 	for line in WLnetsInput:
+		#... but only if they do not begin with a forbidden char.
 		forbidden = False
-		for forbiddenChar in forbiddenLines:
+		for forbiddenChar in FORBIDDEN_LINES:
 			if line.startswith(forbiddenChar):
 				forbidden = True
 		if not forbidden:
 			line = line.strip(' \n')
 			lineSplitted = line.split(' ')
+			# Keep only the first column
 			WLnets.append(lineSplitted[0])
 
+# If the net from the list nets is not found in the list WLnets,
+# add it to the list missingNets.
 for net in nets:
 	try:
 		WLnets.index(net)
 	except:
 		missingNets.append(net)
 
-# print(WLnets)
 print(missingNets)
 
 print len(missingNets),"missing nets in WLnets.out out of", len(nets)
